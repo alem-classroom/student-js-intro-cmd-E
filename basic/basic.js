e6893ca9d9b22dce49b8e23f5548255fc2e69a1f
Kirill <perevyshink_1@mail.ru>
2021-02-23 18:25:26 +0600

    basic task done

--- basic/basic.js
+++ basic/basic.js
@@ -1,48 +1,72 @@
-const number = null
-const float = null
-const str = null
-const bool = null
-const undef = null
+const number = 16
+const float = 16.5
+const str = "Hello"
+const bool = false
+const undef = undefined
 const nullVar = null
 
 const square = (n) => {
-    // your code here
+    return n*n
 }
 
 const hello = (name) => {
-    // your code here
+    return `Hello, ${name}`
 }
 
 const concat = (strings) => {
-    // your code here
+    let str = ""
+    strings.forEach(s => {
+        str += s
+    });
+    return str
 }
 
 const getType = (value) => {
-    // your code here
+    return typeof value
 }
 
 const max = (a, b) => {
-    // your code here
+    if (a > b) return a
+    else return b
 }
 
 const max3 = (a, b, c) => {
-    // your code here
+    if (a > b && a > c) return a
+    else if (b > a && b > c) return b
+    else return c
 }
 
 const change = (N, C, K, L) => {
-    // your code here
+    if (N - K >= 0 && L - C >= 0) return `${N-K}.${L-C}`
+    return 'not enough'
 }
 
 const triangle = (height) => {
-    // your code here
+    let triangle = ''
+    for (let i = 0; i < height; i++) {
+        for (let j = 0; j < i; j++) {
+            triangle += '#'
+        }
+        triangle += '\n'    
+    }
+    triangle += '\n'    
 }
 
 const fizzbuzz = (N) => {
-    // your code here
+    let arr = []
+    for (let index = 1; index <= N; index++) {
+        if (index % 3 === 0) arr.push("Fizz")
+        else if (index % 5 === 0) arr.push("Buzz")
+        else arr.push(index)
+    }
 }
 
-const countChar = (str, char) => {
-    // your code here
+const countChar = (str, char = 'c') => {
+    let count = 0
+    str.forEach(c => {
+        if (c === char) count++
+    });
+    return count
 }
 
 // 🚫 don't edit this export
